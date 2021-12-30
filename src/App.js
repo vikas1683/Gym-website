21359470d87471cee0b515d3dfd00076a7798255
Vikas <vikastevatiya11@gmail.com>
2021-12-30 15:53:28 +0530

    Home page just complited

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 import './App.css';
-import Gym from './Component/Gym';
+import Home from './Component/Home'
+import GymMaterial from './Component/GymMaterial'
+import Products from './Component/Products'
+import Training from './Component/Training'
+import ContactUs from './Component/ContactUs'
+import {BrowserRouter as Router,Link,Routes,Route} from 'react-router-dom'
+
+  
+
+
 
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <div className="app" >
+     
+      <Router>
+       <div className='flex-gap' style={{display:"inline-flex",flexWrap:"wrap",backgroundColor:"black", gap:"3vw",width:"100%",justifyContent:"center",height:"6vw",alignItems:"center",fontFamily:"'Lato', sans-serif",bottom:"0.0vw",position:"relative"}}>
 
-    <Gym />
+       <img style={{position:"relative",width:"4vw",height:"3.5vw"}} src="https://i.postimg.cc/K855qFKB/images.jpg" alt="image" />
+
+      <Link className='link' to="/" style={{color:"lightBlue",textDecoration:"none",fontSize:"1.7vw"}}>Home</Link>  
+      <Link className='link' to="/GymMaterial"style={{color:"lightBlue",textDecoration:"none",fontSize:"1.7vw"}}>GymMaterial</Link>
+      <Link className='link' to="/Training"style={{color:"lightBlue",textDecoration:"none",fontSize:"1.7vw"}}>Training</Link>
+      <Link className='link' to="/ContactUs" style={{color:"lightBlue",textDecoration:"none",fontSize:"1.7vw"}}>Products</Link>
+      <Link className='link' to="/buy" style={{color:"lightBlue",textDecoration:"none",fontSize:"1.7vw"}}>ContactUs</Link>
+
+     
+
+
+      </div>
+      
+      <Routes >
+        <Route path="/" element={<Home />} />
+        <Route path="/GymMaterial" element={<GymMaterial />} />
+        <Route path="/Training" element={<Training />} />
+        <Route path="/ContactUs" element={<ContactUs />} />
+        <Route path="/buy" element={<Products />} />
+      </Routes>
+
+      </Router>
+
+      
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,63 +1,124 @@
-import { Carousel } from 'react-bootstrap'
+import React from 'react'
+import './Style.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Carousel } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
+import ReactPlayer from 'react-player'
 
 
 export default function Home() {
     return (
         <>
-            <div className='home'>
-                <img src="https://i.postimg.cc/zGkPrfn0/91448-fitness-wallpaper-top-free-fitness-background.jpg" alt="" />
-                <img src="https://i.postimg.cc/zGkPrfn0/91448-fitness-wallpaper-top-free-fitness-background.jpg" alt="" />
-                <h1>WELCOME</h1>
-                <h2>The Great Gym</h2>
-            </div>
-            <div className='container'>
-                <Carousel className='first'>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="https://i.postimg.cc/SKz97fcX/6535822.jpg"
-                            alt="First slide"
-                        />
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="https://i.postimg.cc/C12qHVDG/6535838.jpg"
-                            alt="Second slide"
-                        />
+            <div>
 
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="https://i.postimg.cc/2y23f2sS/photo-1595257842044-8f021a58c8a0.jpg"
-                            alt="Third slide"
-                        />
+                <img className='home-image' src="https://i.postimg.cc/DfHfKkc8/boxer-1984344.jpg" alt="image" />
 
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
+                <h1 style={{ position: "relative", fontSize: "6vw", color: "blue", textAlign: "center", right: "20vw", bottom: "45vw" }}>WELCOME</h1>
+                <h2 style={{ position: "relative", fontSize: "6vw", color: "red", textAlign: "center", right: "20vw", bottom: "45vw" }}>FITNESS CENTER</h2>
+                <h4 style={{ position: "relative", fontSize: "6vw", color: "lightGreen", textAlign: "center", right: "20vw", bottom: "45vw" }}>NO PAIN NO GAIN</h4>
+
+
+
+
+                <Button onClick={() => alert("Click [ READ MORE ] Button To Get More Information")} variant="outline-warning" >Click Me</Button>
+
+                <Dropdown style={{ position: "relative", color: "lightGreen", bottom: "2.7vw", right: "18vw", textAlign: "center" }}>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        READ MORE
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">PRODUCTS : Best quality products are available in my gym</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">GYM-MATERIAL : Many machine of the best brand are available in my gym</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">TRAINER : Experience trainer available in my gym</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                <h1 className='h1'> Gym Material OF Best Brand</h1>
+
+                <div className='border'>
+                    <Carousel className='carousel'>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://i.postimg.cc/hjcFnnyR/crossfit-1126999.jpg"
+                                // src="https://i.postimg.cc/sDhqGtN7/2947609.jpg"
+                                alt="First slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>Gym Material Slide</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://i.postimg.cc/YSLWNw5N/2968276.jpg"
+                                alt="Second slide"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Gym Material Slide</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://i.postimg.cc/Gm1Tj1WW/gym-1170496-1280.jpg"
+                                alt="Third slide"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Gym Material Slide</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+
+
+                    </Carousel>
+                </div>
+
+                  <h1 className='h2'>MY GYM MOTIVATIONAL VIDEOS</h1>
+                    <div className='background'>
+                     <div className='player'>
+                    <ReactPlayer className='one' style={{bottom: "22.2vw", position: "relative",left:"2.2vw",transition: 'all 1s ease-in-out'}}
+                        url="https://youtu.be/hlCvQ5rZXjk"
+                        controls
+                        playbackRate={2}
+                        width="45vw"
+                        height="30vw" 
+                />
+  
+                    <ReactPlayer className='two' style={{bottom: "22.2vw", position: "relative" ,left:"4vw",transition: 'all 1s ease-in-out'}}
+                        url="https://youtu.be/v1z7GALM-IY"
+                        controls
+                        playbackRate={2}
+                        width="45vw"
+                        height="30vw" 
+                />
+                
+                </div>
+                     <div className='player-extra'>
+                    <ReactPlayer className='one' style={{bottom: "22.2vw", position: "relative",left:"2.2vw",transition: 'all 1s ease-in-out'}}
+                        url="https://youtu.be/Qxd11sQnZio"
+                        controls
+                        playbackRate={2}
+                        width="45vw"
+                        height="30vw" 
+                />
+  
+                    <ReactPlayer className='two' style={{bottom: "19vw", position: "relative" ,left:"4vw",transition: 'all 1s ease-in-out'}}
+                        url="https://youtu.be/F_kctoYCEfI"
+                        controls
+                        playbackRate={2}
+                        width="45vw"
+                        height="23vw" 
+                />
+                
+                </div>
+                </div>
 
             </div>
         </>
     )
 }
-
-
-
-
-// src="https://i.postimg.cc/Nf3qG369/physical-activity-gym-woman-and-man-work-out-wallpaper-preview.jpg"
-
-// src="https://i.postimg.cc/YSLWNw5N/2968276.jpg"
-
-// src="https://i.postimg.cc/sxT1SVJJ/gym-supplements-background-bodybuilding-pre-workout-whey-protein-powder-pre-workout.jpg"
